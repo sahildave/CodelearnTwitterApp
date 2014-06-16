969f73fa9d11764447465159558adbf6bb9ebaef
sahildave <sahildave1991@gmail.com>
2014-06-16 23:38:36 +0530

    Lesson52 - initial commit

--- src/org/codelearn/twitter/MainActivity.java
+++ src/org/codelearn/twitter/MainActivity.java
@@ -1,57 +1,89 @@
 package org.codelearn.twitter;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.SharedPreferences.Editor;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+
+/*
+ * 
+ * This lesson deals with basic HTTP POST and GET.
+ * Used - HttpUrlConnection, Async, no Json.
+ * When Login button is clicked the username is saved as @usernameString
+ * A post request is generated and the usernameString is sent as the parameter
+ * The user gets back "Hello <usernameString>" as the response, which is 
+ * shown as a Toast. If any exception is caught, different Toast message appears
+ * 
+ */
 public class MainActivity extends Activity {
+
+	String codelearnUrl = ""; // TODO: Add this
 	Button _loginBtn;
+	String passwordString;
+	String usernameString;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		_loginBtn = ( Button ) findViewById(R.id.btn_login);
-		  SharedPreferences prefs = getSharedPreferences("codelearn_twitter", MODE_PRIVATE);
-		  Editor edit = prefs.edit();
+		_loginBtn = (Button) findViewById(R.id.btn_login);
+		SharedPreferences prefs = getSharedPreferences("codelearn_twitter",
+				MODE_PRIVATE);
 
-		String s=prefs.getString("user", null);
-		String s1=prefs.getString("pass", null);
-		if(s!=null && s1!=null)
-		{
-			  Intent intent = new Intent(MainActivity.this, TweetListActivity.class);
-  	          startActivity(intent);
+		String s = prefs.getString("user", null);
+		String s1 = prefs.getString("pass", null);
+		if (s != null && s1 != null) { // TODO: Check this
+			Intent intent = new Intent(MainActivity.this,
+					TweetListActivity.class);
+			startActivity(intent);
 		}
-		
-		
+
 		_loginBtn.setOnClickListener(new View.OnClickListener() {
-		      @Override
-		      public void onClick(View v) {
-		    	  
-	          		EditText username = ( EditText ) findViewById(R.id.fld_username);
-	    			  EditText password = ( EditText ) findViewById(R.id.fld_pwd);		  
-	    			  Log.d("User",username.getText().toString());
-	    			  Log.d("Pass",password.getText().toString());
-	    			  SharedPreferences prefs = getSharedPreferences("codelearn_twitter", MODE_PRIVATE);
-	    			  Editor edit = prefs.edit();
-	    			  edit.putString("user",username.getText().toString());
-	    			  edit.putString("pass",password.getText().toString());
-	    			  
-	    			  edit.commit();
-	    			  Intent intent = new Intent(MainActivity.this, TweetListActivity.class);
-	    			  	          startActivity(intent);
-	    			
-		      }
-		  });
-		
-		
-		
+			@Override
+			public void onClick(View v) {
+
+				EditText username = (EditText) findViewById(R.id.fld_username);
+				EditText password = (EditText) findViewById(R.id.fld_pwd);
+
+				Log.d("User", username.getText().toString());
+				Log.d("Pass", password.getText().toString());
+
+				// SharedPreferences prefs = getSharedPreferences(
+				// "codelearn_twitter", MODE_PRIVATE);
+				// Editor edit = prefs.edit();
+				// edit.putString("user", username.getText().toString());
+				// edit.putString("pass", password.getText().toString());
+				//
+				// edit.commit();
+				// Intent intent = new Intent(MainActivity.this,
+				// TweetListActivity.class);
+				// startActivity(intent);
+
+				usernameString = username.getText().toString();
+				passwordString = username.getText().toString();
+
+				MyAsyncTask async = new MyAsyncTask();
+				async.execute();
+
+			}
+		});
+
 	}
 
 	@Override
@@ -59,6 +91,71 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	private class MyAsyncTask extends AsyncTask<Void, Void, Boolean> {
+
+		String responseString;
+
+		@Override
+		protected Boolean doInBackground(Void... params) {
+
+			try {
+				URL url = new URL(codelearnUrl);
+
+				HttpURLConnection con = (HttpURLConnection) url
+						.openConnection();
+				con.setDoOutput(true);
+				con.setDoInput(true);
+
+				con.setDoOutput(true);
+				DataOutputStream wr = new DataOutputStream(
+						con.getOutputStream());
+				wr.writeBytes(usernameString); // Setting parameters as the
+												// username
+				wr.flush();
+				wr.close();
+
+				BufferedReader br = new BufferedReader(new InputStreamReader(
+						con.getInputStream()));
+				String line;
+				StringBuffer sb = new StringBuffer();
+
+				while ((line = br.readLine()) != null) {
+					sb.append(line + "\n");
+				}
+
+				responseString = sb.toString();
+				br.close();
+
+				return true; // True if no exception occured
+
+			} catch (MalformedURLException e) {
+				e.printStackTrace();
+				return false;
+			} catch (IOException e) {
+				e.printStackTrace();
+				return false;
+			}
+		}
+
+		@Override
+		protected void onPostExecute(Boolean result) {
+			super.onPostExecute(result);
+
+			if (result) { // if no exception occured
+
+				Toast.makeText(MainActivity.this, responseString,
+						Toast.LENGTH_LONG).show();
+
+			} else {
+				Toast.makeText(MainActivity.this,
+						"Uh oh!, Something went wrong", Toast.LENGTH_LONG)
+						.show();
+
+			}
+		}
+
 	}
 
 }
--- src/org/codelearn/twitter/TweetListActivity.java
+++ src/org/codelearn/twitter/TweetListActivity.java
@@ -5,69 +5,61 @@ import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
-import android.os.Bundle;
-import android.app.Activity;
-import android.app.ListActivity;
-import android.content.Intent;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
 import org.codelearn.twitter.models.Tweet;
 
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ListView;
 
 public class TweetListActivity extends ListActivity {
 
+	private TweetAdapter tweetItemArrayAdapter;
+	private List<Tweet> tweets = new ArrayList<Tweet>();
+	private static final String TWEETS_CACHE_FILE = "tweet_cache.ser";
 
-
-    private ArrayAdapter tweetItemArrayAdapter;
-    private List<Tweet> tweets = new ArrayList<Tweet>();
-    private static final String TWEETS_CACHE_FILE = "tweet_cache.ser";
-	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_tweet_list);
-		
-		for ( int i = 0; i < 20; i++ ) {
-		    Tweet tweet = new Tweet();
-		    tweet.setTitle("A nice header for Tweet # " +i);
-		    tweet.setBody("Some random body text for the tweet # " +i);
-		    tweets.add(tweet);
+
+		for (int i = 0; i < 20; i++) {
+			Tweet tweet = new Tweet();
+			tweet.setTitle("A nice header for Tweet # " + i);
+			tweet.setBody("Some random body text for the tweet # " + i);
+			tweets.add(tweet);
 		}
-		
+
 		FileOutputStream fos = null;
-	    ObjectOutputStream oos = null;
-	    
-	    try {
-	        fos = openFileOutput(TWEETS_CACHE_FILE, MODE_PRIVATE);
-	        oos = new ObjectOutputStream(fos);
-	        oos.writeObject(tweets);
-	        Log.d("codelearn", "Successfully wrote tweets to the file.");
-	    } 
-	    catch ( Exception e ) {
+		ObjectOutputStream oos = null;
 
-	        Log.e("codelearn", "Failed to write to serialised file.", e);
+		try {
+			fos = openFileOutput(TWEETS_CACHE_FILE, MODE_PRIVATE);
+			oos = new ObjectOutputStream(fos);
+			oos.writeObject(tweets);
+			Log.d("codelearn", "Successfully wrote tweets to the file.");
+		} catch (Exception e) {
 
-	    } finally {
-	        
-	        try {
-	            fos.close();
-	        } catch (Exception e) {
-	            Log.e("codelearn", "Error closing file stream", e);
-	        }
-	        
-	        try {
-	            oos.close();
-	        } catch (Exception e) {
-	            Log.e("codelearn", "Error closing output stream", e);
-	        }
-	    }
+			Log.e("codelearn", "Failed to write to serialised file.", e);
 
-		
+		} finally {
+
+			try {
+				fos.close();
+			} catch (Exception e) {
+				Log.e("codelearn", "Error closing file stream", e);
+			}
+
+			try {
+				oos.close();
+			} catch (Exception e) {
+				Log.e("codelearn", "Error closing output stream", e);
+			}
+		}
+
 		tweetItemArrayAdapter = new TweetAdapter(this, tweets);
 		setListAdapter(tweetItemArrayAdapter);
 	}
@@ -80,8 +72,8 @@ public class TweetListActivity extends ListActivity {
 	}
 
 	@Override
-	 protected void onListItemClick(ListView l, View v, int position, long id) {
+	protected void onListItemClick(ListView l, View v, int position, long id) {
 		Intent intent = new Intent(this, TweetDetailActivity.class);
-	     startActivity(intent);
-	 }
+		startActivity(intent);
+	}
 }
